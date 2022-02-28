0682a707277bbb6015129c60260d49de4557026f
LaisPerpetuo <laisperpetuo9@gmail.com>
2022-02-28 22:20:40 +0000

    setup inicial airflow e spark

---
+++ setup.sh
@@ -0,0 +1,29 @@
+export SPARK_HOME=/workspace/datapipeline/spark-3.2.1-bin-hadoop3.2
+export AIRFLOW_HOME=~/airflow
+
+AIRFLOW_VERSION=2.0.2
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+
+
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+pip install apache-airflow-providers-apache-spark
+
+# initialize the database
+airflow db init
+
+airflow users create \
+    --username admin \
+    --firstname Lais \
+    --lastname Perpetuo \
+    --role Admin \
+    --email laisperpetuo9@gmail.com
+
+# start the web server, default port is 8080
+airflow webserver --port 8080 -D
+
+# start scheduler
+airflow scheduler
